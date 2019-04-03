49d929eee8db8521d5eefc3a1c982f61942beb36
Yeimer Molina <yeimer.molina@gmail.com>
2019-04-03 10:54:39 -0500

    Redirect user on logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,7 +19,7 @@ module.exports = app => {
   app.get("/api/logout", (req, res) => {
     //this function is added by passport on the req object
     req.logout();
-    res.send(req.user);
+    res.redirect("/");
   });
 
   app.get("/api/current_user", (req, res) => {
