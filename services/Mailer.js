4f558cd5d549254ba2457e6837c6e662ae06900d
Yeimer Molina <yeimer.molina@gmail.com>
2019-04-05 10:39:57 -0500

    Update Mailer and survey template

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,53 +1,30 @@
-const sendgrid = require("sendgrid");
-const helper = sendgrid.mail;
+const sgMail = require("@sendgrid/mail");
+// const sendgrid = require("sendgrid");
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {
+sgMail.setApiKey(keys.sendGridKey);
+
+class Mailer {
   constructor({ subject, recipients }, content) {
-    super();
-
-    this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email("no-replay@feebackapp.com");
-    this.subject = subject;
-    this.body = new helper.Content("text/html", content);
-    this.recipients = this.formatAddresses(recipients);
-
-    //addContent method comes from helper.Mail
-    this.addContent(this.body);
-    this.addClickTracking();
-    this.addRecipients();
+    this.message = {
+      from: "no-reply@feedbackapp.com",
+      to: this.formatAddresses(recipients),
+      subject: subject,
+      content: [
+        {
+          type: "text/html",
+          value: content
+        }
+      ]
+    };
   }
 
   formatAddresses(recipients) {
-    return recipients.map(({ email }) => new helper.Email(email));
-  }
-
-  addClickTracking() {
-    // this is from sendgrid documentation
-    const trackingSettings = new helper.TrackingSettings();
-    const clickTracking = new helper.ClickTracking(true, true);
-
-    trackingSettings.setClickTracking(clickTracking);
-    this.addTrackingSettings(trackingSettings);
-  }
-
-  addRecipients() {
-    const personalize = new helper.Personalization();
-
-    this.recipients.forEach(recipient => {
-      personalize.addTo(recipient);
-    });
-    this.addPersonalization(personalize);
+    return recipients.map(({ email }) => email);
   }
 
   async send() {
-    const request = this.sgApi().emptyRequest({
-      method: "POST",
-      path: "/v3/mail/send",
-      body: this.toJSON()
-    });
-
-    const response = await this.sgApi.API(request);
+    const response = await sgMail.send(this.message);
     return response;
   }
 }
